8881c6a7434805056cf1706611f1bd47d8b20915
KBB665 <smalamen@gmail.com>
2026-02-01 21:20:01 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,4 +1,4 @@
 sed -i '86d' padavan-ng/trunk/build_firmware.sh
-sed -i 's/^CONFIG_RT_SINGLE_SKU=y*/# CONFIG_RT_SINGLE_SKU is not set/' padavan-ng/trunk/configs/boards/ASUS/RT-AC51U/kernel-3.4.x.config
+sed -i 's/^CONFIG_RT_SINGLE_SKU=y*/# CONFIG_RT_SINGLE_SKU is not set/' padavan-ng/trunk/configs/boards/TPLINK/TL_C2-V1/kernel-3.4.x.config
 apt update
 apt install -y gcc-multilib
